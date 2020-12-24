dbdd8bef543db7d7251752a6cc4082d3b2264f73
Alexander Stein <alexander.stein@gsa.gov>
2020-12-24 14:45:27 -0500

    Update ETL scripts parameterization.

--- scripts/etl.sh
+++ scripts/etl.sh
@@ -1,27 +1,37 @@
 #!/usr/bin/env sh
 
 export PATH=/usr/local/bin:${PATH}
+export NVD_START_YEAR=${NVD_START_YEAR:-2002}
+export GOST_LINUX_DISTROS="debian redhat"
+export OVAL_ALPINE_VERSIONS=${OVAL_ALPINE_VERSIONS:-"3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10"}
+export OVAL_AMAZON_VERSIONS=${OVAL_AMAZON_VERSIONS:-""}
+export OVAL_DEBIAN_VERSIONS=${OVAL_DEBIAN_VERSIONS:-"8 9 10"}
+export OVAL_REDHAT_VERSIONS=${OVAL_REDHAT_VERSIONS:-"6 7 8"}
+export OVAL_UBUNTU_VERSIONS=${OVAL_UBUNTU_VERSIONS:-"16 18 20"}
 
-echo Load go-cve-dictionary data ...
-
-for i in `seq 2016 $(date +"%Y")`; do \
-    echo Starting year $i ...
-    go-cve-dictionary fetchnvd ${@} -years $i; \
+for y in `seq $NVD_START_YEAR $(date +"%Y")`; do \
+    echo Load go-cve-dictionary data for year $y...
+    go-cve-dictionary fetchnvd -years $y ${@}; \
 done
 
 echo Load go-cve-dictionary-data ...
 go-exploitdb ${@} fetch exploitdb
 
-echo Load gost data ...
-gost fetch ${@} debian
-gost fetch ${@} redhat
+for d in GOST_LINUX_DISTROS; do \
+    echo Load gost data for distro $d ...
+    gost fetch ${@} $d; \
+done
 
 echo Load go-msfdb data ...
 go-msfdb ${@} fetch msfdb
 
-echo Load goval-dictionary data ...
-goval-dictionary fetch-redhat ${@} 6 7 8 
-goval-dictionary fetch-amazon ${@}
-goval-dictionary fetch-debian ${@} 8 9 10
-goval-dictionary fetch-ubuntu ${@} 16 18 20
-goval-dictionary fetch-alpine ${@} 3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10
+echo Load goval-dictionary data for Alpine Linux ...
+goval-dictionary fetch-alpine ${@} $OVAL_ALPINE_VERSIONS  #3.3 3.4 3.5 3.6 3.7 3.8 3.9 3.10
+echo Load goval-dictionary data for Amazon Linux ...
+goval-dictionary fetch-amazon ${@} $OVAL_AMAZON_VERSIONS  #
+echo Load goval-dictionary data for Debian Linux ...
+goval-dictionary fetch-debian ${@} $OVAL_DEBIAN_VERSIONS  #8 9 10
+echo Load goval-dictionary data for RedHat Linux ...
+goval-dictionary fetch-redhat ${@} $OVAL_REDHAT_VEERSIONS #6 7 8 
+echo Load goval-dictionary data for Ubuntu Linux ...
+goval-dictionary fetch-ubuntu ${@} $OVAL_UBUNTU_VERSIONS  #16 18 20
