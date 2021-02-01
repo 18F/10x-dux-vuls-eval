ea656282e9f1597f385f0831915163072bc380d7
Alexander Stein <alexander.stein@gsa.gov>
2021-01-31 22:17:19 -0500

    Last Dockerfile updates and script improvements.

---
+++ scripts/s3restore.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+set -e -o pipefail
+
+export AWS_S3_BUCKET=$(echo $VCAP_SERVICES | jq -r .s3[0].credentials.bucket)
+export AWS_DEFAULT_REGION=$(echo $VCAP_SERVICES | jq -r .s3[0].credentials.region)
+export AWS_ACCESS_KEY_ID=$(echo $VCAP_SERVICES | jq -r .s3[0].credentials.access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(echo $VCAP_SERVICES | jq -r .s3[0].credentials.secret_access_key)
+
+aws s3 cp s3://${AWS_S3_BUCKET}/backup.sql - | psql $DATABASE_URL
